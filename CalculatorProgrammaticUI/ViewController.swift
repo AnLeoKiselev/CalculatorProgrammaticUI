d92026684af3fe4d1f4a741622bd804d66776169
AnLeoKiselev <anleokiselev@gmail.com>
2022-10-29 11:47:44 +0300

    Initial Commit

---
+++ CalculatorProgrammaticUI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CalculatorProgrammaticUI
+//
+//  Created by Anatoliy on 29.10.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
